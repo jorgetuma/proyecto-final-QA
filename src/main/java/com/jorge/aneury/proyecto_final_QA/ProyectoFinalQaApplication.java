44b17ce4a07b30b292c197327ae92a507bba49b5
jorgetuma <jetm0001@ce.pucmm.edu.do>
2024-07-02 23:50:00 -0400

    Empenzando el proyecto final

---
+++ src/main/java/com/jorge/aneury/proyecto_final_QA/ProyectoFinalQaApplication.java
@@ -0,0 +1,13 @@
+package com.jorge.aneury.proyecto_final_QA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalQaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalQaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jorge/aneury/proyecto_final_QA/ProyectoFinalQaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorge.aneury.proyecto_final_QA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalQaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
