d5679132c10d65105a7e8cd0c1dc372410885081
jorgetuma <jetm0001@ce.pucmm.edu.do>
2024-07-23 16:11:03 -0400

    Migrando proyecto a gradle

--- src/test/java/com/jorge/aneury/proyecto_final_QA/ProductoTest.java
+++ src/test/java/com/jorge/aneury/proyecto_final_QA/ProductoTest.java
@@ -1,9 +1,9 @@
 package com.jorge.aneury.proyecto_final_QA;
 
 import com.jorge.aneury.proyecto_final_QA.entidades.Producto;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ProductoTest {
 
